be229e2b7f18a6376375ce7cb577eae781cf92d7
Rob <30297803+rdanielsstat@users.noreply.github.com>
2026-03-07 15:51:47 -0600

    fix: change SQL date functions to be compatible with Redshift and Postgres

--- medicare_dbt/models/marts/mart_enrollment_national.sql
+++ medicare_dbt/models/marts/mart_enrollment_national.sql
@@ -4,7 +4,27 @@ with staging as (
 
 national as (
     select
+        {% if target.type == 'redshift' %}
+        dateadd(month,
+            case month
+                when 'January'   then 0
+                when 'February'  then 1
+                when 'March'     then 2
+                when 'April'     then 3
+                when 'May'       then 4
+                when 'June'      then 5
+                when 'July'      then 6
+                when 'August'    then 7
+                when 'September' then 8
+                when 'October'   then 9
+                when 'November'  then 10
+                when 'December'  then 11
+            end,
+            date_from_parts(year, 1, 1)
+        ) as report_date,
+        {% else %}
         make_date(year, to_number(to_char(to_date(month, 'Month'), 'MM'), '99')::integer, 1) as report_date,
+        {% endif %}
         total_beneficiaries,
         original_medicare_benes,
         medicare_advantage_benes,
