4ba9f840e83f26d737dda032d3dbd85e11b000de
Rob <30297803+rdanielsstat@users.noreply.github.com>
2026-03-08 18:28:08 -0500

    fix: fix rounding error by casting for marts

--- medicare_dbt/models/marts/mart_enrollment_by_state.sql
+++ medicare_dbt/models/marts/mart_enrollment_by_state.sql
@@ -14,8 +14,8 @@ by_state as (
         disabled_beneficiaries,
         male_beneficiaries,
         female_beneficiaries,
-        ROUND(medicare_advantage_benes / NULLIF(total_beneficiaries, 0) * 100, 1) AS ma_penetration_rate,
-        ROUND(original_medicare_benes / NULLIF(total_beneficiaries, 0) * 100, 1)  AS ffs_penetration_rate        
+        ROUND((medicare_advantage_benes / NULLIF(total_beneficiaries, 0) * 100)::numeric, 1) AS ma_penetration_rate,
+        ROUND((original_medicare_benes / NULLIF(total_beneficiaries, 0) * 100)::numeric, 1)  AS ffs_penetration_rate,
     from staging
     where bene_geo_lvl = 'State'
     and month = 'Year'
--- medicare_dbt/models/marts/mart_enrollment_national.sql
+++ medicare_dbt/models/marts/mart_enrollment_national.sql
@@ -30,8 +30,8 @@ national as (
         disabled_beneficiaries,
         male_beneficiaries,
         female_beneficiaries,
-        ROUND(medicare_advantage_benes / NULLIF(total_beneficiaries, 0) * 100, 1) AS ma_penetration_rate,
-        ROUND(original_medicare_benes / NULLIF(total_beneficiaries, 0) * 100, 1)  AS ffs_penetration_rate,
+        ROUND((medicare_advantage_benes / NULLIF(total_beneficiaries, 0) * 100)::numeric, 1) AS ma_penetration_rate,
+        ROUND((original_medicare_benes / NULLIF(total_beneficiaries, 0) * 100)::numeric, 1)  AS ffs_penetration_rate,
         year,
         month
       from staging
