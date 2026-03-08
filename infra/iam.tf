5348dabdaf77ff90d86227e5b2216413b8153a68
Rob <30297803+rdanielsstat@users.noreply.github.com>
2026-03-08 09:09:47 -0500

    fix: update tofu s3 settings

--- infra/iam.tf
+++ infra/iam.tf
@@ -84,13 +84,14 @@ resource "aws_iam_role" "redshift_s3_role" {
 resource "aws_iam_role_policy" "redshift_s3_policy" {
   name = "${var.project}-redshift-s3-policy"
   role = aws_iam_role.redshift_s3_role.id
-
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
       Action = [
         "s3:GetObject",
+        "s3:PutObject",
+        "s3:DeleteObject",
         "s3:ListBucket"
       ]
       Resource = [
