59fe81be9f1b5e3330f2ee90e7a99eeebcd149a0
Rob <30297803+rdanielsstat@users.noreply.github.com>
2026-03-08 11:05:33 -0500

    fix: remove summing in state enrollment mart

--- medicare_dbt/models/marts/mart_enrollment_by_state.sql
+++ medicare_dbt/models/marts/mart_enrollment_by_state.sql
@@ -2,21 +2,21 @@ with staging as (
     select * from {{ ref('stg_medicare_enrollment') }}
 ),
 by_state as (
-    select 
+    select
         year,
         state,
         state_name,
         fips_code,
-        sum(total_beneficiaries)        as total_beneficiaries,
-        sum(original_medicare_benes)    as original_medicare_benes,
-        sum(medicare_advantage_benes)   as medicare_advantage_benes,
-        sum(aged_beneficiaries)         as aged_beneficiaries,
-        sum(disabled_beneficiaries)     as disabled_beneficiaries,
-        sum(male_beneficiaries)         as male_beneficiaries,
-        sum(female_beneficiaries)       as female_beneficiaries
-      from staging
-     where bene_geo_lvl = 'State'
-       and month = 'Year'
+        total_beneficiaries,
+        original_medicare_benes,
+        medicare_advantage_benes,
+        aged_beneficiaries,
+        disabled_beneficiaries,
+        male_beneficiaries,
+        female_beneficiaries
+    from staging
+    where bene_geo_lvl = 'State'
+    and month = 'Year'
 )
 select * from by_state
 order by year, state
